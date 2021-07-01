e7d5d04aca9423e0134240ca2d0d7ff95f4696c1
MrVab <vaibhavrajputvns@gmail.com>
2021-07-01 15:44:55 +0530

    Update cron.service.js

--- cornimplement/services/cron.service.js
+++ cornimplement/services/cron.service.js
@@ -3,11 +3,11 @@ var i = 0;
 function dateScheduler(date, month, timezone) {
   month -= 1;
   var job = new CronJob(
-    `1-10 23 14 ${date} ${month} *`,
+    `1 24 15 ${date} ${month} *`,
     () => {
       console.log("You will see this message on ", date, "/", month + 1, i++),
         timezone;
-      /// preform the op
+      // preform the op below
     },
     null,
     true,
